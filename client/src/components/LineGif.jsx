3661d0438b214dc2f53f410a7ae0194152b46f82
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-04-29 22:48:15 +0900

    Design: line gif

---
+++ client/src/components/LineGif.jsx
@@ -0,0 +1,9 @@
+const LineGif = () => {
+  return (
+    <div style={{ height: "100px", backgroundColor: "var(--main-color)" }}>
+      cheese pot gif 들어갈 자리
+    </div>
+  );
+};
+
+export default LineGif;
