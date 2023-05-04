894abb9cd0e4c298e0a517e54bbe5c6484b1ea3a
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-05-04 18:10:52 +0900

    Refactor: 선택화면 페이지 css -> styled-compoent

--- client/src/pages/selectPage.jsx
+++ client/src/pages/selectPage.jsx
@@ -1,46 +1,89 @@
 import React from "react";
-import { useState } from "react";
-import "./selectPage.css";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import CatelogyItem from "../components/List/CatelogyItem";
 import Navbar from "../components/Navbar";
-import SelectBox from "../components/SelectBox";
-import MediaItem from "../components/MediaItem";
+import MediaItem from "../components/List/MediaItem";
+import SelectBox from "../components/Card/SelectBox";
+import DetailAlart from "../components/Card/DetailAlart";
+import Info from "../components/Button/Info";
+import ReverseBtn from "../components/Button/ReverseBtn";
 
-const containerStyle = {
-  padding: "0 23rem",
-  fontFamily: "GmarketSansLight",
-};
-const Main = () => {
-  const [count, setCount] = useState(0);
+const Container = styled.div`
+  background-color: black;
+  height: 120%;
+`;
+
+const Main = styled.div`
+  padding: 0 37rem;
+  font-family: GmarketSansLight;
+`;
+
+const SubNavbar = styled.div`
+  display: flex;
+  margin-top: 13rem;
+  align-items: center;
+`;
+
+const Selectors = styled.div`
+  display: flex;
+  margin-left: auto;
+  gap: 2rem;
+`;
+const MediaContainer = styled.div`
+  margin-top: 5.5rem;
+  display: flex;
+  row-gap: 4.5rem;
+  column-gap: 1.2rem;
+  flex-wrap: wrap;
+`;
+
+const SelectPage = () => {
+  const { pathname } = useLocation();
+  const [count, setCount] = useState(5); // 총 본 영화 갯수가 몇인지
+
+  useEffect(() => {
+    // 페이지 이동후 스크롤을 가장 위로 올림.
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
-    <div className="entire-page">
+    <Container>
       <Navbar />
-      <div style={containerStyle}>
+      <Main>
         <h1 className="h1-style">MOVIE</h1>
-        <div
-          style={{ display: "flex", marginTop: "13rem", alignItems: "center" }}
-        >
+        <SubNavbar>
           <h2>
-            <span className="bold">{count}</span> MOVIES
+            <span
+              style={{
+                fontSize: "3.2rem",
+                fontWeight: "bold",
+              }}
+            >
+              {count}
+            </span>
+            MOVIES
           </h2>
-          <div style={{ display: "flex", marginLeft: "auto", gap: "20px" }}>
+          <Selectors>
             <SelectBox />
             <SelectBox />
             <SelectBox />
-          </div>
-        </div>
-        <div className="container-item">
+          </Selectors>
+        </SubNavbar>
+        <MediaContainer>
           {/* 향후 db에서 map함수 사용 */}
+          <MediaItem onClick={() => {}} />
           <MediaItem />
           <MediaItem />
           <MediaItem />
           <MediaItem />
-          <MediaItem />
-        </div>
-      </div>
-    </div>
+        </MediaContainer>
+      </Main>
+      <DetailAlart />
+      <Info />
+    </Container>
   );
 };
 
-export default Main;
+export default SelectPage;
